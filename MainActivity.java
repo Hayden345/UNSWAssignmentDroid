479917c3d5f03670d23bcca9dcd11b436317af1f
Tom975 <32894740+Tom975@users.noreply.github.com>
2017-11-05 20:19:34 +1100

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,11 +1,10 @@
-package com.example.z5056635.assignment;
+package com.example.dylan.taxtimewithfriends;
 
-import android.content.ContentValues;
-import android.database.sqlite.SQLiteDatabase;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.example.z5056635.assignment.Databases.DriverTable;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void logTripActivity(View v){
+        startActivity(new Intent(MainActivity.this, logTrip.class));
+    }
+
+    public void addDriverActivity (View v){
+        startActivity(new Intent(MainActivity.this, addDriver.class));
+    }
+
+    public void addVehicleActivity (View v){
+        startActivity(new Intent(MainActivity.this, addVehicle.class));
+    }
+
+    public void addExpenseActivity (View v){
+        startActivity(new Intent(MainActivity.this, addExpense.class));
+    }
+
+    public void summaryActivity (View v){
+        startActivity(new Intent(MainActivity.this, summary.class));
     }
 }
